0f38abd6ec65c485dd58193bae222b5eebea53a7
KateDeBruyn <1444800@students.wits.ac.za>
2022-08-11 14:39:57 +0200

    Pages and placeholders
    Progress on pages. Added index, blogs, style guide, data visualisation and data art pages and placeholders. Also added a .js to get the back to top button working and an additional general .css file.

---
+++ javascript.js
@@ -0,0 +1,3 @@
+// To Top button
+// Tutorial by Filip: 6 JavaScript examples to make your websites more ENGAGING! (https://www.youtube.com/watch?v=MCRV9ajSfoc)
+const toTop = () => window.scrollTo({top: 0, behavior: "smooth"});
