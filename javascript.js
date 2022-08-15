07df0b95e4ce3125ff8ab075f149900bcd45596d
KateDeBruyn <1444800@students.wits.ac.za>
2022-08-15 14:44:53 +0200

    Final Draft 2

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
 // To Top button
 // Tutorial by Filip: 6 JavaScript examples to make your websites more ENGAGING! (https://www.youtube.com/watch?v=MCRV9ajSfoc)
 const toTop = () => window.scrollTo({top: 0, behavior: "smooth"});
+
+//Animations
+document.documentElement.style.setProperty('--animate-duration', '2s');
